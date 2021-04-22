013df30ce14828774a71b92dc0047864d9b7e373
joshua fernando <jay.fernando.m@gmail.com>
2021-04-22 17:17:15 +0800

    hahahah

--- class/config.php
+++ class/config.php
@@ -2,12 +2,12 @@
 
 class config {
     private $user ='root';
-    private $password ='admin123';
+    private $password ='';
     public $pdo = null;
 
     public function con() {
       try {
-          $this->pdo = new PDO('mysql:host=localhost;dbname=todolist', $this->user, $this->password);
+          $this->pdo = new PDO('mysql:host=127.0.0.1;dbname=todolist;port=3307', $this->user, $this->password);
       } catch (PDOException $e) {
          die($e);
       }
